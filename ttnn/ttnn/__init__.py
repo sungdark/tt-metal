473d977a5c9cd7b8c65f76733b197e326f1a8779
Ryan Miller <rmiller@tenstorrent.com>
2026-02-25 03:48:20 +0000

    Export symbols in ttnn

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -266,6 +266,8 @@ from ttnn.types import (
     MeshProgramDescriptor,
     merge_program_descriptors,
     cb_descriptor_from_sharded_tensor,
+    UnpackToDestMode,
+    compute_program_descriptor_hash,
     TensorAccessorArgs,
 )
 
